73756e8ad4992e1d46cfa2c93b6730b3703851a5
Dharanish A M <dharanish816@gmail.com>
2024-09-06 21:06:13 +0530

    Deleted Project

--- TOWNTROLLEY/client/src/components/BecomeSeller.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function BecomeSeller(){
-    return(
-    <section>
-
-    </section>
-    )
-}
--- TOWNTROLLEY/client/src/components/Sell.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Sell(){
-    return(
-    <section>
-
-    </section>
-    )
-}
